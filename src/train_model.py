5821801d920b794f9983d2e92a7128663edede34
lramaye <loic.ramaye.abo@icloud.com>
2025-10-12 14:39:38 +0400

    update train model regression

--- src/train_model.py
+++ src/train_model.py
@@ -1,6 +1,6 @@
 import sklearn
 import pandas as pd
-from sklearn import ensemble
+from sklearn.ensemble import RandomForestRegressor
 import numpy as np
 import bentoml
 from bentoml.io import NumpyNdarray
@@ -12,16 +12,16 @@ y_test = pd.read_csv('data/processed/y_test.csv')
 y_train = np.ravel(y_train)
 y_test = np.ravel(y_test)
 
-rf_classifier = ensemble.RandomForestClassifier(n_jobs = -1)
+rf_regressor = RandomForestRegressor(n_jobs=-1, random_state=42)
 
 #--Train the model
-rf_classifier.fit(X_train, y_train)
+rf_regressor.fit(X_train, y_train)
 
 #--Test the model
-rf_classifier.predict(X_test)
+rf_regressor.predict(X_test)
 
 #--Get the model accuracy
-accuracy = rf_classifier.score(X_test, y_test)
+accuracy = rf_regressor.score(X_test, y_test)
 
 print(f"Model accuracy: {accuracy}")
 
@@ -30,9 +30,9 @@ test_data = X_test.iloc[[0]]
 # print the actual label
 print(f"Actual label: {y_test[0]}")
 # print the predicted label
-print(f"Predicted label: {rf_classifier.predict(test_data)[0]}")
+print(f"Predicted label: {rf_regressor.predict(test_data)[0]}")
 
 # Enregistrer le modèle dans le Model Store de BentoML
-model_ref = bentoml.sklearn.save_model("accidents_rf", rf_classifier)
+model_ref = bentoml.sklearn.save_model("accidents_rf", rf_regressor)
 
 print(f"Modèle enregistré sous : {model_ref}")
